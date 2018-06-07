ccffa43ffbf1b72541b97703af6a0a6491af01b2
Aspha9 <ucdeugen916@gmail.com>
2018-06-07 13:57:33 +0200

    add react app

--- main.js
+++
@@ -1,7 +0,0 @@
-//Bootstrap and jquery included via CodePen. Much js functionality is in minified Bootstrap.js and configured through data attributes.
-
-$(document).ready(function() {
-    //Set the made-by and social buttons to the same height, for vertical alignment purposes. 
-    var Height = $('#social-links').height();
-    $('#made-by').height(Height);
-});
